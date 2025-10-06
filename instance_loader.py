8d8057637db82eb3a582bbce43526104e8cf5406
Jay Bhan <jaybhan@mit.edu>
2025-10-05 23:50:22 -0400

    push

--- instance_loader.py
+++ instance_loader.py
@@ -87,12 +87,12 @@ class InstanceLoader(object):
     #end
 
     def get_batches(self, batch_size):
-        print(f"DEBUG: Total files: {len(self.filenames)}")
-        print(f"DEBUG: Expected batches: {len(self.filenames) // batch_size}")
+        print("DEBUG: Total files: {}".format(len(self.filenames)))
+        print("DEBUG: Expected batches: {}".format(len(self.filenames) // batch_size))
         for i in range( len(self.filenames) // batch_size ):
-            print(f"DEBUG: Generating batch {i}")
+            print("DEBUG: Generating batch {}".format(i))
             instances = list(self.get_instances(batch_size))
-            print(f"DEBUG: Instances: {instances}")
+            print("DEBUG: Instances: {}".format(instances))
             yield InstanceLoader.create_batch(instances)
         #end
     #end
